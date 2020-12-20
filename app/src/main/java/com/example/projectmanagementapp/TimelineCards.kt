dcaec14f5ca9429ea3a757768a02b16a62825abb
pomoranca <armin_x@hotmail.com>
2020-12-20 12:10:28 +0100

    Add items to home layout

---
+++ app/src/main/java/com/example/projectmanagementapp/TimelineCards.kt
@@ -0,0 +1,189 @@
+package com.example.projectmanagementapp
+
+
+import android.graphics.DashPathEffect
+import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.dp
+
+val linePosition = 80.dp
+val timelineDotSize = 8.dp
+val dashPathEffect = DashPathEffect(floatArrayOf(20f, 10f), 0f)
+
+val DrawScope.topRight: Offset get() = Offset(size.width, 0f)
+val DrawScope.bottomRight: Offset get() = Offset(size.width, size.height)
+
+enum class LineState(val color: Color, val stroke: Stroke) {
+    Undefined(Color.Gray, Stroke(width = 1f, pathEffect = dashPathEffect)), // dotted line
+    In(Color.White, Stroke(width = 8f)), // solid white
+    Out(Color.Gray, Stroke(width = 1f)) // solid gray
+}
+
+fun Modifier.drawLine(
+    status: Status?,
+    top: LineState,
+    bottom: LineState
+) = drawBehind {
+    val centerPosition = Offset(x = size.width, y = size.height / 2f)
+    drawLine(
+        color = top.color,
+        start = topRight,
+        end = centerPosition,
+        strokeWidth = top.stroke.width,
+        pathEffect = top.stroke.pathEffect
+    )
+    drawLine(
+        color = bottom.color,
+        start = centerPosition,
+        end = bottomRight,
+        strokeWidth = bottom.stroke.width,
+        pathEffect = bottom.stroke.pathEffect
+    )
+    if (status != null) {
+        drawCircle(
+            color = Color.White,
+            radius = timelineDotSize.toPx(),
+            center = centerPosition
+        )
+        drawCircle(
+            color = status.color,
+            radius = (timelineDotSize - 2.dp).toPx(),
+            center = centerPosition
+        )
+    }
+}
+
+@Composable
+fun TimelineRow(
+    status: Status?,
+    topLineState: LineState,
+    bottomLineState: LineState,
+    leftContent: @Composable () -> Unit,
+    rightContent: @Composable () -> Unit
+) {
+    Layout({
+        Row(
+            Modifier
+                .padding(end = 16.dp)
+                .drawLine(status, topLineState, bottomLineState),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            leftContent()
+        }
+        Row {
+            rightContent()
+        }
+    }) { (left, right), constraints ->
+        val leftWidth = linePosition.toIntPx()
+        val placedRight = right.measure(
+            androidx.compose.ui.unit.Constraints(
+                minWidth = (constraints.minWidth - leftWidth).coerceAtLeast(0),
+                maxWidth = (constraints.maxWidth - leftWidth).coerceAtLeast(0),
+                minHeight = constraints.minHeight,
+                maxHeight = constraints.maxHeight
+            )
+        )
+        val placedLeft = left.measure(
+            androidx.compose.ui.unit.Constraints.fixed(
+                leftWidth,
+                placedRight.height
+            )
+        )
+        layout(placedLeft.width + placedRight.width, placedRight.height) {
+            placedLeft.placeRelative(0, 0)
+            placedRight.placeRelative(leftWidth, 0)
+        }
+    }
+}
+
+@Composable
+fun TimelineHeader() {
+    TimelineRow(
+        status = null,
+        topLineState = LineState.Undefined,
+        bottomLineState = LineState.Undefined,
+        leftContent = {
+            Text("Data")
+        }
+    ) {
+        Text("Tasks")
+        Text("Show in days")
+    }
+}
+
+
+@Composable
+fun TimelineTask(
+    task: Task,
+    topLineState: LineState,
+    bottomLineState: LineState,
+) {
+    TimelineRow(
+        status = task.status,
+        topLineState = topLineState,
+        bottomLineState = bottomLineState,
+        leftContent = {
+            Text(task.timeCode)
+        }
+    ) {
+        Column(
+            Modifier
+                .padding(vertical = 8.dp)
+                .background(Color.White, RoundedCornerShape(6.dp))
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    task.status.humanReadable,
+                    color = task.status.color
+                )
+                Text(task.tag)
+            }
+            Row {
+                Text(task.title)
+            }
+            Row {
+                IconButton(onClick = {}) {
+                    Row {
+                        Icon(Icons.Default.MailOutline)
+                        Text("${task.commentCount}")
+                    }
+                }
+                IconButton(onClick = {}) {
+                    Row {
+                        Icon(Icons.Default.Email)
+                        Text("${task.attachmentCount}")
+                    }
+                }
+                Spacer(Modifier.weight(1f))
+                Text("N\u00B0 ${task.id}")
+                AvatarList(
+                    modifier = Modifier,
+                    32.dp,
+                    task.assignees,
+                    onUserClick = { }
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/projectmanagementapp/TimelineScreen.kt
+++ app/src/main/java/com/example/projectmanagementapp/TimelineScreen.kt
@@ -1,103 +1,390 @@
 package com.example.projectmanagementapp
 
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.AmbientDensity
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.projectmanagementapp.ui.typography
 import dev.chrisbanes.accompanist.coil.CoilImage
+import kotlin.math.roundToInt
+
+enum class Status(val color: Color, val humanReadable: String) {
+    New(Color(0xFF2CC09C), "New"),
+    InProgress(Color(0xFFF26950), "In Progress"),
+    Review(Color.Red, "Review"),
+    Done(Color(0xFF5A55CA), "Done"),
+}
+
+class Project(
+    val id: Int,
+    val title: String,
+    val date: String,
+    val days: Int,
+    val status: Status,
+    val progress: Float,
+    val users: List<User>,
+    val tasks: List<Task>
+)
+
+class User(
+    val id: Int,
+    val name: String,
+) {
+    fun imageUrlForSize(size: Int) = "https://i.pravatar.cc/100?img=$id"
+}
+
+class Task(
+    val id: Int,
+    val timeCode: String,
+    val title: String,
+    val tag: String,
+    val status: Status,
+    val assignees: List<User>,
+    val commentCount: Int,
+    val attachmentCount: Int,
+)
+
+val zachary = User(
+    id = 2,
+    name = "Zachary Butler",
+)
+val mary = User(
+    id = 3,
+    name = "Mary Brown",
+)
+val sarah = User(
+    id = 4,
+    name = "Sarah Murphy"
+)
+val mockProject = Project(
+    id = 1,
+    title = "Create additional pages",
+    date = "Dec 18, 2019",
+    days = 3,
+    status = Status.InProgress,
+    progress = 0.85f,
+    users = listOf(mary, sarah, zachary),
+    tasks = listOf(
+        Task(
+            id = 163,
+            timeCode = "24.19",
+            title = "Contact page",
+            tag = "#Design",
+            status = Status.InProgress,
+            assignees = listOf(zachary),
+            commentCount = 3,
+            attachmentCount = 5,
+        ),
+        Task(
+            id = 158,
+            timeCode = "24.19",
+            title = "Calculator page",
+            tag = "#Design",
+            status = Status.Done,
+            assignees = listOf(sarah, mary),
+            commentCount = 8,
+            attachmentCount = 2,
+        ),
+        Task(
+            id = 157,
+            timeCode = "23.19",
+            title = "Technical Task",
+            tag = "#Frontend",
+            status = Status.Review,
+            assignees = listOf(zachary),
+            commentCount = 4,
+            attachmentCount = 8,
+        ),
+        Task(
+            id = 159,
+            timeCode = "23.19",
+            title = "Calculator page",
+            tag = "#Backend",
+            status = Status.Done,
+            assignees = listOf(mary),
+            commentCount = 4,
+            attachmentCount = 6,
+        ),
+        Task(
+            id = 163,
+            timeCode = "22.19",
+            title = "Contact page",
+            tag = "#Design",
+            status = Status.InProgress,
+            assignees = listOf(zachary),
+            commentCount = 3,
+            attachmentCount = 5,
+        ),
+        Task(
+            id = 158,
+            timeCode = "22.19",
+            title = "Calculator page",
+            tag = "#Design",
+            status = Status.Done,
+            assignees = listOf(sarah, mary),
+            commentCount = 8,
+            attachmentCount = 2,
+        ),
+        Task(
+            id = 157,
+            timeCode = "21.19",
+            title = "Technical Task",
+            tag = "#Frontend",
+            status = Status.Review,
+            assignees = listOf(zachary),
+            commentCount = 4,
+            attachmentCount = 8,
+        ),
+        Task(
+            id = 159,
+            timeCode = "21.19",
+            title = "Calculator page",
+            tag = "#Backend",
+            status = Status.Done,
+            assignees = listOf(mary),
+            commentCount = 4,
+            attachmentCount = 6,
+        ),
+    )
+)
 
 
 @Composable
 fun TimelineScreen(project: Project = mockProject) {
-    Column {
+    ScrollableColumn {
         Column(Modifier.padding(horizontal = 20.dp, vertical = 16.dp)) {
             Text(project.title, style = typography.h1)
             Row {
-                Text(text = "${project.days} days", style = typography.body2)
-                Text(
-                    text = "|",
-                    modifier = Modifier.padding(horizontal = 4.dp),
-                    style = typography.body2
-                )
-                Text(text = project.date, style = typography.body2)
+                Text("${project.days} days", style = typography.body2)
+                Text("|", modifier = Modifier.padding(horizontal = 4.dp), style = typography.body2)
+                Text(project.date, style = typography.body2)
             }
-
-            AvatarList(users = project.users)
+            Row(
+                Modifier.padding(top = 16.dp).fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                AvatarList(
+                    size = 44.dp,
+                    users = project.users,
+                    showAddButton = true,
+                    onAddClick = {
+                        // TODO
+                    },
+                    onUserClick = {
+                        // TODO
+                    }
+                )
+                ProjectProgressIndicator(
+                    progress = project.progress,
+                    status = project.status
+                )
+            }
         }
+
         Column(
             Modifier
                 .background(Color(0xFFF1F5FE), shape = RoundedCornerShape(topLeft = 40.dp))
                 .padding(top = 40.dp)
                 .padding(horizontal = 20.dp)
+                .fillMaxWidth()
         ) {
-            Text(text = "Hello world")
-
+            TimelineHeader()
+            for (index in project.tasks.indices) {
+                val task = project.tasks[index]
+                val prev = project.tasks.getOrNull(index - 1)
+                val next = project.tasks.getOrNull(index + 1)
+                TimelineTask(
+                    task = task,
+                    topLineState = when {
+                        prev == null -> LineState.Undefined
+                        task.timeCode == prev.timeCode -> LineState.In
+                        else -> LineState.Out
+                    },
+                    bottomLineState = when {
+                        next == null -> LineState.Undefined
+                        task.timeCode == next.timeCode -> LineState.In
+                        else -> LineState.Out
+                    },
+                )
+            }
         }
     }
 }
 
-/** Custom modifier for avatar layout offset
- *
- * */
+
+private val stroke = Stroke(8f, cap = StrokeCap.Butt)
+fun Modifier.circularProgress(
+    progress: Float,
+    primaryColor: Color,
+) = this.drawBehind {
+    drawCircularIndicator(
+        startAngle = 270f,
+        sweep = progress * 360f,
+        color = primaryColor,
+        stroke = stroke,
+    )
+}
+
+
+@Composable
+fun ProjectProgressIndicator(
+    modifier: Modifier = Modifier,
+    progress: Float,
+    status: Status
+) {
+    val text = "${(progress * 100).roundToInt()}%"
+    val color = status.color
+    Box(
+        modifier
+            .circularProgress(progress, color)
+            .size(48.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text, color = color, fontWeight = FontWeight.Bold)
+    }
+}
+
+private fun DrawScope.drawCircularIndicator(
+    startAngle: Float,
+    sweep: Float,
+    color: Color,
+    stroke: Stroke
+) {
+    // To draw this circle we need a rect with edges that line up with the midpoint of the stroke.
+    // To do this we need to remove half the stroke width from the total diameter for both sides.
+    val diameterOffset = stroke.width / 2
+    val arcDimen = size.width - 2 * diameterOffset
+    drawArc(
+        color = color,
+        startAngle = startAngle,
+        sweepAngle = sweep,
+        useCenter = false,
+        topLeft = Offset(diameterOffset, diameterOffset),
+        size = Size(arcDimen, arcDimen),
+        style = stroke
+    )
+    if (sweep < 360) {
+        drawArc(
+            color = color.copy(alpha = 0.3f),
+            startAngle = (startAngle + sweep) % 360,
+            sweepAngle = 360 - sweep,
+            useCenter = false,
+            topLeft = Offset(diameterOffset, diameterOffset),
+            size = Size(arcDimen, arcDimen),
+            style = stroke
+        )
+    }
+}
+
+
 private fun Modifier.layoutOffset(x: Dp = 0.dp, y: Dp = 0.dp) =
     this then layout { measurable, constraints ->
         val placeable = measurable.measure(constraints)
         layout(placeable.width + x.toIntPx(), placeable.height + y.toIntPx()) {
             placeable.placeRelative(x.toIntPx(), y.toIntPx())
-
         }
     }
 
 @Composable
-fun AvatarList(users: List<User>) {
-    Row {
+fun AvatarList(
+    modifier: Modifier = Modifier,
+    size: Dp,
+    users: List<User>,
+    showAddButton: Boolean = false,
+    onUserClick: (User) -> Unit,
+    onAddClick: () -> Unit = {}
+) {
+    Row(modifier) {
         users.forEachIndexed { index, user ->
             Avatar(
+                modifier = modifier,
+                size,
                 user,
-                modifier = Modifier
-                    /** By using custom layoutOffset modifier, size of items that are drawn on screen are same as actual items that are shown
-                     * whereas if we use modifier.offset(x = if(index > 0) -30.dp * index else 0.dp, items drawn will not calculate the minus offset */
-//                    .offset(x = if (index > 0) -30.dp * index else 0.dp)
-                    .layoutOffset(if (index == 0) 0.dp else -10.dp)
-
-
+                onClick = { onUserClick(user) },
+                stack = index != 0
+            )
+        }
+        if (showAddButton) {
+            AvatarAddButton(
+                modifier = modifier,
+                size,
+                onClick = onAddClick,
+                stack = users.isNotEmpty()
             )
         }
     }
 }
 
 @Composable
-fun Avatar(user: User, modifier: Modifier = Modifier) {
-    CoilImage(
-        user.imageUrlForSize(with(AmbientDensity.current) { 40.dp.toIntPx() }),
+fun AvatarAddButton(
+    modifier: Modifier = Modifier,
+    size: Dp,
+    onClick: () -> Unit,
+    stack: Boolean = false,
+) {
+    Avatar(modifier, size, onClick, stack) {
+        Icon(Icons.Filled.Add)
+    }
+}
+
+@Composable
+fun Avatar(
+    modifier: Modifier = Modifier,
+    size: Dp,
+    onClick: () -> Unit,
+    stack: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val overlap = -size / 5
+    IconButton(
+        onClick = onClick,
         modifier = modifier
+            .layoutOffset(if (!stack) 0.dp else overlap)
             .shadow(5.dp, CircleShape, clip = false)
             .background(Color.White, CircleShape)
-            .padding(4.dp)
-            .clip(CircleShape)
-            .size(48.dp)
+            .padding(2.dp)
+            .size(size),
+        content = content
     )
 }
 
-
-
-
-
-
-
-
-
+@Composable
+fun Avatar(
+    modifier: Modifier = Modifier,
+    size: Dp,
+    user: User,
+    onClick: () -> Unit,
+    stack: Boolean = false
+) {
+    Avatar(modifier, size, onClick, stack) {
+        CoilImage(
+            user.imageUrlForSize(with(AmbientDensity.current) { size.toIntPx() }),
+            modifier = Modifier
+                .clip(CircleShape)
+                .fillMaxSize()
+        )
+    }
+}
